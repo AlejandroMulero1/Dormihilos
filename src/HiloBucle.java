26d935e45f8162c54ee8ee655166707c1d8671e6
AlejandroMulero1 <alejandro.mulero@iesnervion.es>
2022-11-15 14:39:14 +0100

    Initial commit

---
+++ src/HiloBucle.java
@@ -0,0 +1,16 @@
+public class HiloBucle extends Thread {
+
+    @Override
+    public void run() {
+
+        while (true) {
+            System.out.println("Soy el bucle " + this.getName() + " y estoy trabajando");
+            int numAleatorio = (int) (Math.random() * 10000) + 1000;
+            try {
+                Thread.sleep(numAleatorio);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
